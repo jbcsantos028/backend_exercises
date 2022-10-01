bd61841318598dfecee003fed70178860bfcd4eb
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-01 12:02:02 +0800

    modified db_joins

--- db_joins.sql
+++ db_joins.sql
@@ -1,3 +1,29 @@
+-- creating and updating students table (from previous activity)
+CREATE TABLE students (
+	id integer PRIMARY KEY, 
+	first_name character varying(255) NOT NULL,
+	middle_name character varying(255) NOT NULL, 
+	last_name character varying(255) NOT NULL,
+	age integer NOT NULL,
+	location character varying(255) NOT NULL
+);
+
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+VALUES 	(1, 'Juan', '', 'Cruz', 18, 'Manila'),
+		    (2, 'Anne', '', 'Wall', 20, 'Manila'),
+		    (3, 'Theresa', '', 'Joseph', 21, 'Manila'),
+		    (4, 'Issac', '', 'Gray', 19, 'Laguna'),
+		    (5, 'Zack', '', 'Matthews', 22, 'Marikina'),
+		    (6, 'Finn', '', 'Lam', 25, 'Manila');
+
+--SELECT * from students;
+
+UPDATE students SET first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan' WHERE id = 1;
+
+DELETE FROM students WHERE id = 6;
+-- end of line for the creation and updating of the students table
+
+-- start of code for creating the notes table
 CREATE TABLE notes (
 	id integer PRIMARY KEY,
 	body text,
