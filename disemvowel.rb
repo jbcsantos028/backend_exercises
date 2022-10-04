df300b0e4890b01dc2f1c107e1a1bb0eeed64403
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-04 19:30:05 +0800

    modified disemvowel.rb v.3

--- disemvowel.rb
+++ disemvowel.rb
@@ -1,11 +1,11 @@
 def disemvowel(string)
   new_arr = []
+  vowel = ['a', 'e', 'i', 'o', 'u']
   
   string.each_char do |char|
-    unless char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U'
-      new_arr << char
-    end
+    new_arr << char unless vowel.include?(char.downcase)
   end
+
   new_arr.join('')
 end
 
