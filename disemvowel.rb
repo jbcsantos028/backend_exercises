368885d24021877d659364cbe01c8e655d19cc38
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-04 19:24:33 +0800

    modified disemvowel.rb ver 2

--- disemvowel.rb
+++ disemvowel.rb
@@ -1,12 +1,11 @@
 def disemvowel(string)
   new_arr = []
   
-  string.downcase.each_char do |char|
-    unless char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'
+  string.each_char do |char|
+    unless char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U'
       new_arr << char
     end
   end
-  
   new_arr.join('')
 end
 
