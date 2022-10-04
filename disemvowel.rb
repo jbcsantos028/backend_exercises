44579f49c480b3fb0ddf048df0fa132470edbc3a
jbcsantos028 <jbcsantos028@gmail.com>
2022-10-04 19:18:14 +0800

    disemvowel.rb

---
+++ disemvowel.rb
@@ -0,0 +1,11 @@
+def disemvowel(string)
+  new_arr = []
+  string.each_char do |char|
+    unless char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U'
+      new_arr << char
+    end
+  end
+  new_arr.join('')
+end
+
+p disemvowel("This website is for losers LOL!")
